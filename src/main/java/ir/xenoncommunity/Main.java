0d48a83581cd01c7a2346baaf2582e4d9f1b93f4
XyPz.op <90617105+RealSuperUser@users.noreply.github.com>
2023-12-31 00:05:10 +0330

    Add files via upload

---
+++ src/main/java/ir/xenoncommunity/Main.java
@@ -0,0 +1,10 @@
+package ir.xenoncommunity;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Main{
+	public void main(final String[] args){
+		MainRunner.run();
+	}
+}
---
+++ src/main/java/ir/xenoncommunity/MainRunner.java
@@ -0,0 +1,10 @@
+package ir.xenoncommunity;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class MainRunner{
+	public void run(){
+		System.out.println("salam sina");
+	}
+}
